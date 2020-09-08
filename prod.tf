12ba615ba30ce306067ae258e1fda1ee27ce2740
Mohit Juneja <mohit0920@icloud.com>
2020-09-08 17:05:38 +0530

    load balancer and  subnets

--- prod.tf
+++ prod.tf
@@ -33,6 +33,22 @@ resource "aws_security_group" "prod_web" {
     "Terraform" : "true"
   }
 }
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "ap-northeast-1a"
+  
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    "Terraform": "true"
+  } 
+}
   
 resource "aws_instance" "prod_web" {
   count = 2  
@@ -60,3 +76,20 @@ resource "aws_eip" "prod_web" {
   }
 }
 
+resource "aws_elb" "prod_web" {
+  name 		  = "prod-web"
+  instances 	  = aws_instance.prod_web.*.id
+  subnets 	  = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  security_groups = [aws_security_group.prod_web.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port	      = 80
+    lb_protocol       = "http"
+  }
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
